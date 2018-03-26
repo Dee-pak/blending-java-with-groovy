7a95661a966c221ed3cbb04af04fd0ed95653074
Deepak Khobragade <Khobragade.d@husky.neu.edu>
2018-03-25 23:02:52 -0500

    Adding Grapes Example

---
+++ src/com/groovy/tutorial/GrapesExample.groovy
@@ -0,0 +1,17 @@
+/*
+    Grape is a JAR dependency manager embedded into Groovy. Grape lets you quickly add maven repository dependencies to your classpath, 
+    making scripting even easier. The simplest use is as simple as adding an annotation to your script.
+    http://groovy-lang.org/grape.html
+*/
+
+// Simple example using apache commons lang library. This will allow you to send a script to anyone without worrying about script's dependencies.
+// @Grab => for single dependency, @Grapes([abc, xyz]) => for multiple dependencies
+
+@Grapes(
+    @Grab(group='org.apache.commons', module='commons-lang3', version='3.7')
+)
+import org.apache.commons.lang3.text.WordUtils
+
+String name = "Deepak Khobragade"
+WordUtils wordUtils = new WordUtils();
+println wordUtils.initials(name)
