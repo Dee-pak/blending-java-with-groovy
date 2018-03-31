92591972b59361f307b62bc1ffa4f82660daf3eb
Deepak Khobragade <deepakkhobragade@a-v33akhtd8.carfax.cfx>
2018-03-31 02:11:47 -0500

    Modifying listsInGroovy.groovy file

--- src/com/groovy/tutorial/collections/listsInGroovy.groovy
+++ src/com/groovy/tutorial/collections/listsInGroovy.groovy
@@ -12,7 +12,7 @@
 // We can create an empty List using '[]' operator
 def mainList = [1,2,3,4,5,3,7,3,1,5,7,8,9,6]
 println mainList
-println mainList    // By default groovy creates and Instance of ArrayList
+println mainList.class   // By default groovy creates and Instance of ArrayList
 
 // We can create instance of another collection using the 'as <Collection>' keyword as follows
 def linkedList = [] as LinkedList
@@ -28,11 +28,45 @@ def list3 = list1.clone() // clone can also be called
 assert list1 == list3
 
 assert mainList[-2] == 9    // We can use negative indexes to count from the end
+assert mainList.getAt(-3) == 8    // Negative indexes can also be used with getAt() method
 
+// Lists can also be evaluated as boolean expressions
 
-// add | remove | get | clear operations
+assert ![]    // empty list would assert to false
+assert [1] && ['a'] && [null]    // non empty list including null would assert to true
 
-mainList.push(28)    // appends 28 at the end of the list
-mainList.putAt(0, 29)    //inserts 29 at index 0
-mainList[1] = 93
-println mainList
+// Iterating over a list can be done with traditional for loops, each, eachWithIndex.
+
+[1,2,3,4,5,6,7].each {
+    print "Item : $it"    // 'it' is an implicit parameter corresponding to the current element
+}
+
+[1,2,3,4,5,6,7,8].eachWithIndex { it, i ->    // 'it' is the current element, while 'i' is the current index
+    print "Element : $it, Index : $i"
+}
+
+// In addition to traversing we can also create a new list by transforming each of its element into something else using collect method from Groovy.
+assert [1,2,3,4,5].collect { it * 5 } == [5,10,15,20,25]
+
+// short syntax instead of collect
+assert [1,2,3]*.multiply(2) == [2,4,6]
+
+// It is also possible to give collect method a list as an argument which would collect the elements
+def collectorList = []
+[1,2,3].collect(collectorList) { it + 2 }
+assert [3,4,5] == collectorList
+
+/*
+    Manupulating Lists
+*/
+
+// Filtering and Searching
+assert [1,2,3].find { it > 2 } == 3   // Would return the first element which matches the criteria
+assert [9,8,7,6,5,4].findAll { it > 5 } == [9,8,7,6]
+assert ['a', 'b', 'c', 'd', 'e'].findIndexOf {
+    it in ['d', 'e']    // finds index of 1st element in matching criteria
+} == 3
+
+assert ['a', 'b', 'c', 'd', 'e'].indexOf('c') == 2    // index of 'c' is returned
+assert ['a', 'b', 'c', 'd', 'e'].indexOf('z') == -1    // index -1 means value is not in the list
+assert ['a', 'e', 'b', 'c', 'd', 'e'].lastIndexOf('e') == 5
