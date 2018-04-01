840d0317a4a6a64259b2a772a74f1883f2939d42
Deepak Khobragade <deepakkhobragade@a-v33akhtd8.carfax.cfx>
2018-04-01 00:57:35 -0500

    Moving forward with Lists

--- src/com/groovy/tutorial/collections/listsInGroovy.groovy
+++ src/com/groovy/tutorial/collections/listsInGroovy.groovy
@@ -9,12 +9,13 @@
 
 */
 
-// We can create an empty List using '[]' operator
+// ----------- We can create an empty List using '[]' operator ----------- //
+def emptyList = []
 def mainList = [1,2,3,4,5,3,7,3,1,5,7,8,9,6]
 println mainList
 println mainList.class   // By default groovy creates and Instance of ArrayList
 
-// We can create instance of another collection using the 'as <Collection>' keyword as follows
+// We can create instance of another implementation of List using the 'as <Collection>' keyword as follows
 def linkedList = [] as LinkedList
 println linkedList.class
 
@@ -30,12 +31,12 @@ assert list1 == list3
 assert mainList[-2] == 9    // We can use negative indexes to count from the end
 assert mainList.getAt(-3) == 8    // Negative indexes can also be used with getAt() method
 
-// Lists can also be evaluated as boolean expressions
+// ----------- Lists can also be evaluated as boolean expressions ----------- //
 
 assert ![]    // empty list would assert to false
 assert [1] && ['a'] && [null]    // non empty list including null would assert to true
 
-// Iterating over a list can be done with traditional for loops, each, eachWithIndex.
+// ----------- Iterating over a list can be done with traditional for loops, each, eachWithIndex. ----------- //
 
 [1,2,3,4,5,6,7].each {
     print "Item : $it"    // 'it' is an implicit parameter corresponding to the current element
@@ -57,12 +58,12 @@ def collectorList = []
 assert [3,4,5] == collectorList
 
 /*
-    Manupulating Lists
+   -----------  Manupulating Lists ----------- 
 */
 
 // Filtering and Searching
 assert [1,2,3].find { it > 2 } == 3   // Would return the first element which matches the criteria
-assert [9,8,7,6,5,4].findAll { it > 5 } == [9,8,7,6]
+assert [9,8,7,6,5,4].findAll { it > 5 } == [9,8,7,6]    // Will maintain the order
 assert ['a', 'b', 'c', 'd', 'e'].findIndexOf {
     it in ['d', 'e']    // finds index of 1st element in matching criteria
 } == 3
@@ -70,3 +71,77 @@ assert ['a', 'b', 'c', 'd', 'e'].findIndexOf {
 assert ['a', 'b', 'c', 'd', 'e'].indexOf('c') == 2    // index of 'c' is returned
 assert ['a', 'b', 'c', 'd', 'e'].indexOf('z') == -1    // index -1 means value is not in the list
 assert ['a', 'e', 'b', 'c', 'd', 'e'].lastIndexOf('e') == 5
+
+assert [1,2,3,4].every { it < 5 }    // assert true if EVERY element matches the predicate
+assert ![9,8,7,6,5].every { it < 2 }
+
+assert [1,2,3,4,9].any { it > 8 }    // would assert true if ANY of the element meets the predicate
+assert ![9,8,7,6,5,1].any{ it < 1 }    
+
+assert [10,20,30].sum() == 60    // can call sum() method to sum the elements of the list
+assert ['a', 'b', 'c', 'd', 'e'].sum() { ((char)it) - ((char)'a') } == 10
+assert [1,2,3].sum(1000) == 1006    // an initial value can be provided
+
+assert [1,2,3].join('-') == '1-2-3'    // Strings can be joined with a delimiter
+
+assert ['a','b','c'].inject('Reduced : ') { injectedString, listItem -> 
+    println "$injectedString + $listItem = ${injectedString + listItem}"
+    injectedString + listItem    // reduce operation
+} == 'Reduced : abc'
+
+assert [1,2,3].inject(10) { injectedValue, listItem -> 
+    println "$injectedValue + $listItem = ${injectedValue + listItem}"    // inject() method is similar to operation 'total = total + something
+    injectedValue + listItem
+} == 16
+
+
+// Using inject with custom objects
+class Person {
+    String username
+    String email
+}
+def persons = [
+        new Person(username:'deepack', email:'deepak@gmail.com'),
+        new Person(username :'nand', email :'nand@gmail.com')
+]
+/*
+    Converting a list to a map where the key is the value
+    username of Person and the value is the email
+    of Person. We inject an empty map as the starting
+    point for the result.
+*/
+def map = persons.inject([:]) { result, person ->
+    result[person.username] = person.email
+     return result
+}
+println map
+
+
+// --------- Adding and removing elements from the list -----------    //
+
+def list = []
+assert list.empty
+
+list << 5
+assert list.size == 1
+
+list << 3 << 'i' << 11
+assert list == [5,3,'i',11]
+
+list << ['d', 'e']
+assert list == [5,3,'i',11, ['d', 'e']]
+
+assert ([1,2] << 6 << 7 << ['i','j']) == [1,2,6,7,['i', 'j']]    // first item in the chain of << is the target list
+
+assert ([1,2,3] << 4) == [1,2,3].leftShift(4)    // using left shift is equivalent to << operator
+
+assert [1,2] + 3 + [4,5] + 6 == [1,2,3,4,5,6]
+assert [1,2].plus(3).plus([4,5]).plus(6) == [1,2,3,4,5,6]   // Note : '+' or plus() are not mutative operations compared to '<<', i.e. it will create and return a new list everytime
+
+def a = [1,2,3]
+a += 4                // creates a new list and assigns it to list 'a'
+a += [5,6]            // everytime
+assert a == [1,2,3,4,5,6]
+
+assert [1,2,*[3,4,5],6,7,*[8,*[9,10],11],12] == [1,2,3,4,5,6,7,8,9,10,11,12]    // '*' is known as spreader which flattens the list
+assert [1,2,[3,4,5],6,7,[8,[9,10],11],12].flatten() == [1,2,3,4,5,6,7,8,9,10,11,12]
